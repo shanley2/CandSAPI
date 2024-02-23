f1c69a36d6b7ec736509eaba7a3f47f9b07afb9c
shanley2 <shanley907@gmail.com>
2024-02-23 16:39:45 -0500

    Added Country and State Models

--- csapi/models.py
+++ csapi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    id = models.IntegerField()
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=60)
+
+class State(models.Model):
+    id = models.IntegerField()
+    country_id = models.IntegerField()
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=60)
+
